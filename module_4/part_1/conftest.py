c6210d58adf98f7ec129691a27f62eb02460d867
Indira Stepanova <m_indira_i@hotmail.com>
2021-06-10 07:19:37 +0300

    module 4 part 1 changes

--- module_4/part_1/conftest.py
+++ module_4/part_1/conftest.py
@@ -1,37 +1,29 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                 help="Choose language: ru, en-GB, es, fr")
+                 help="Choose language: ru, en-gb, es, fr")
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                 help="Choose browser: chrome or firefox")
 
+@pytest.fixture(scope="session")
+def language(request):
+    return request.config.getoption("language")
+
+@pytest.fixture(scope="session")
+def browser(request):
+    return request.config.getoption("browser_name")
 
 @pytest.fixture(scope="function")
-def browser(request):
-    language = request.config.getoption("language")
-    browser = None
-    if language == "ru":
-        print("\nstart ru url for test..")
-        browser = webdriver.Chrome()
-        browser.implicitly_wait(5)
-        browser.get("http://selenium1py.pythonanywhere.com/ru/")
-    elif language == "en-GB":
-        print("\nstart en-gb url for test..")
-        browser = webdriver.Chrome()
-        browser.implicitly_wait(5)
-        browser.get("http://selenium1py.pythonanywhere.com/en-gb/")
-    elif language == "es":
-        print("\nstart es url for test..")
-        browser = webdriver.Chrome()
-        browser.implicitly_wait(5)
-        browser.get("http://selenium1py.pythonanywhere.com/es/")
-    elif language == "fr":
-        print("\nstart fr url for test..")
-        browser = webdriver.Chrome()
-        browser.implicitly_wait(5)
-        browser.get("http://selenium1py.pythonanywhere.com/fr/")
-    else:
-        raise pytest.UsageError("--language should be ru, en-GB, es, fr")
+def browser(language):
+    options=Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language, 'intl.' = browser_name})
+    browser=webdriver.Chrome(options=options)
     yield browser
-    print("\nquit browser..")
     browser.quit()
+
+
--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,19 +1,37 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-
+#Common
+main_page_link = "http://selenium1py.pythonanywhere.com/"
 search_login_link_locator = "#login_link"
 search_redirect_link_locator = "li > ul > li:nth-child(1) > a"
+search_language_of_page = ".no-js"
+
+#Locators of good
 search_good_locator = "a[title='Coders at Work']"
+
+#Add to Basket
 search_add_to_basket_locator = "button.btn-add-to-basket"
 
-browser = None
+#dict for add to basket button
+expected_button_text = {
+    "ru": "Добавить в корзину",
+    "en-gb": "Add to basket", 
+    "fr": "Ajouter au panier",
+    "es": "Añadir al carrito"
+}
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_login_link(browser, language):
+    #Arrange
+    browser.implicitly_wait(5)
+    browser.get(main_page_link)
+    #Assert
     browser.find_element_by_css_selector(search_redirect_link_locator).click()
     browser.find_element_by_css_selector(search_good_locator).click()
+    add_to_basket = browser.find_element_by_css_selector(search_add_to_basket_locator)
+    language_of_page = browser.find_element_by_css_selector(search_language_of_page).get_attribute("lang")
+    #Assert
+    assert language_of_page == language, "The language of page does not match the locale!"
+    assert add_to_basket.text in expected_button_text[language], "Incorrect text!"
     assert EC.element_to_be_clickable((By.CSS_SELECTOR, search_add_to_basket_locator)), "Button is not available!"
-    assert EC.text_to_be_present_in_element(search_add_to_basket_locator, "Добавить в корзину" or "Add to basket" or
-                                            "Ajouter au panier" or "Añadir al carrito"), "Incorrect text!"
+    
     
