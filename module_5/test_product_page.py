92cd0d86ebc83b088ce51a7f162b2e20befddff7
Indira Stepanova <m_indira_i@hotmail.com>
2021-06-23 23:37:12 +0300

    autoformat

--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -10,41 +10,41 @@ link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wi
 @pytest.mark.login_guest
 class TestLoginFromProductPage():
     def test_guest_should_see_login_link_on_product_page(self, browser):
-        #Arrange
+        # Arrange
         page = ProductPage(browser, link)
-        #Act
+        # Act
         page.open()
-        #Assert
+        # Assert
         page.should_be_login_link()
 
     def test_guest_can_go_to_login_page_from_product_page(self, browser):
-        #Arrange
+        # Arrange
         page = ProductPage(browser, link)
         page.open()
-        #Act
+        # Act
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
-        #Assert
+        # Assert
         login_page.should_be_login_page()
 
 
 @pytest.mark.empty_basket
 class TestProductPage:
     def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser, language):
-        #Arrange
+        # Arrange
         page = ProductPage(browser, link)
         page.open()
-        #Act
+        # Act
         page.go_to_basket_page()
         basket_page = BasketPage(browser, browser.current_url)
-        #Assert
+        # Assert
         basket_page.should_be_basket_page(language)
 
 
 @pytest.mark.add_to_basket
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
-    #Arrange
+    # Arrange
     def setup(self, browser):
         self.page = LoginPage(browser, link)
         self.page.open()
@@ -56,18 +56,18 @@ class TestUserAddToBasketFromProductPage():
 
     def test_user_cant_see_success_message(self, browser):
         promo_page = ProductPage(browser, link)
-        #Act
+        # Act
         promo_page.open()
-        #Assert
+        # Assert
         # Проверяем, что нет сообщения об успехе с помощью is_not_element_present:
         promo_page.should_not_be_success_message()
 
     def test_user_can_add_product_to_basket(self, browser, language):
-        #Arrange
+        # Arrange
         promo_page = ProductPage(browser, link)
         promo_page.open()
-        #Act
+        # Act
         promo_page.add_product_to_basket()
-        #Assert
+        # Assert
         promo_page.should_be_add_to_basket_button(language)
         promo_page.compare_selected_and_added_product()
